c377f01ab03dce556b51a6f8c32aeb3a8c4796c3
Tobias <tobiasjbraun@gmail.com>
2020-10-08 18:40:37 -0700

    Update Startup.ino
    added more speceficity to check for continuity on chute charge in startup

--- Main_code_Mk_1.0/Startup.ino
+++ Main_code_Mk_1.0/Startup.ino
@@ -1,10 +1,16 @@
 
 int startup() {
   int nextState = 1;
-  checkContinuity(); // should return a boolean to indicate if a failure state needs to be called
-  armed = digitalRead(armingPin);
-  if (armed) {
-    nextState = 2
+  digitalWrite(chuteChargeContOut, HIGH;
+  int charge1 = digitalRead(chuteCharge1);
+  int charge2 = digitalRead(chuteCharge2); 
+  digitalWrite(chuteChargeContOut, LOW);
+  int armed = digitalRead(armingPin);
+
+  if ((not charge1) or (not charge2)){
+    nextState - 0; // Placeholder for failure state at least one of the chute charges is not connected
+  } else if (armed) {
+    nextState = 2;
   }
   return nextState;
-  }
+}
