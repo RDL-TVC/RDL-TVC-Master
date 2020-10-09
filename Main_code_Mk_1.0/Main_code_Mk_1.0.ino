831ca3832708bef26f6644fb05b18c0ac5352c80
Tobias <tobiasjbraun@gmail.com>
2020-10-08 17:11:31 -0700

    Update Main_code_Mk_1.0.ino
    Added the basic loop structure to the main file

--- Main_code_Mk_1.0/Main_code_Mk_1.0.ino
+++ Main_code_Mk_1.0/Main_code_Mk_1.0.ino
@@ -1,9 +1,45 @@
+
+const int armingPin = 2; // Place holder pin for the arming button
+int currentState = 0; // State of the state machine to know which flight function to call. Starts at startup.
+
 void setup() {
-  // put your setup code here, to run once:
+  // Initializing all Pins
+
+  pinMode(armingPin, INPUT);
+
+  // Initializing sensors
+  initializeSensors();
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+
+  currentState = callFLightFunc(currentState); // added a function rapper to make more modular, however unlikely to be needed
 
 }
+
+int callFLightFunc(int state) {
+  /* 
+  Intakes the current state of the state machine and runs the appropriate function for that state.
+  Returns the next state of the state machine
+  */
+  int nextState = state;
+  if (state == 1) {
+    nextState = startup();
+  } else if (state == 2) {
+    nextState = groundidle();
+  } else if (state == 3) {
+    nextState = liftoff();
+  } else if (state == 4) {
+    nextState = burnout();
+  } else if (state == 5) {
+    nextState = freefall();
+   }else if (state == 6) {
+    nextState = chute();
+  } else if (state == 7) {
+    nextState = landing();
+  } else {
+    nextState = failure(); // General failure state might need to specify different failure states.
+  }
+  return nextState;
+  }
