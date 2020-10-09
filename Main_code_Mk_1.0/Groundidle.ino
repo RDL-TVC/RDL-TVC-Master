4ec21f421f82414cff36f2ebbd173697b07d8675
Tobias <tobiasjbraun@gmail.com>
2020-10-08 18:39:55 -0700

    Create Groundidle.ino
    created the ground idle flight function skeleton.

---
+++ Main_code_Mk_1.0/Groundidle.ino
@@ -0,0 +1,17 @@
+
+
+int groundidle() {
+  int nextState = 2;
+
+  array alt = altitued();
+  array orient = orientation();
+  
+  datalog(alt, orient, millis());
+  
+  if (orient[2] >= accelThreshold) { // TODO: placeholder values that need to be changed once data format has been determined.
+    nextState = 3;
+    initPID();
+    unlockServos();
+  }
+  return nextState;
+}
