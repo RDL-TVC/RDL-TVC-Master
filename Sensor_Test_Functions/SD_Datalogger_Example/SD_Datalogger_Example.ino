1e3e971052609ffc3181c4ce03bc1d6f96db7ff1
evancg01 <72581437+evancg01@users.noreply.github.com>
2021-01-12 15:44:31 -0700

    Update SD_Datalogger_Example.ino
    I just commented out the lines for the Audio Shield in the sampel code

--- Sensor_Test_Functions/SD_Datalogger_Example/SD_Datalogger_Example.ino
+++ Sensor_Test_Functions/SD_Datalogger_Example/SD_Datalogger_Example.ino
@@ -52,8 +52,8 @@ const int chipSelect = 4;
 void setup()
 {
   //UNCOMMENT THESE TWO LINES FOR TEENSY AUDIO BOARD:
-  SPI.setMOSI(7);  // Audio shield has MOSI on pin 7
-  SPI.setSCK(14);  // Audio shield has SCK on pin 14
+  //SPI.setMOSI(7);  // Audio shield has MOSI on pin 7
+  //SPI.setSCK(14);  // Audio shield has SCK on pin 14
   
  // Open serial communications and wait for port to open:
   Serial.begin(9600);
