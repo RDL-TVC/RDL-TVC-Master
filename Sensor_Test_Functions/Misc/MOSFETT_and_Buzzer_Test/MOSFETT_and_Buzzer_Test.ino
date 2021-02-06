e26878ed18de0b431ae0084093e79aec47e0ae3d
DrEvilOwl <72581426+DrEvilOwl@users.noreply.github.com>
2021-02-06 00:43:22 -0700

    BMP generic Test Code, and a code to test buzzer and mosfett
    Mosfett code NOT working

---
+++ Sensor_Test_Functions/Misc/MOSFETT_and_Buzzer_Test/MOSFETT_and_Buzzer_Test.ino
@@ -0,0 +1,20 @@
+int piezoPin = 14;
+
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(4, OUTPUT);
+  pinMode(5, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  tone(piezoPin,2000,500);
+  
+  digitalWrite(4,HIGH);
+  digitalWrite(5,HIGH);
+  delay(5000);
+  digitalWrite(4,LOW);
+  digitalWrite(5,LOW);
+  delay(5000);
+}
