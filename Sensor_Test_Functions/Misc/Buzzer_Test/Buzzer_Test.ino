f45364246f5345004330db477ede48b37096a022
DrEvilOwl <72581426+DrEvilOwl@users.noreply.github.com>
2021-02-20 15:15:49 -0700

    Created Buzzer Test Function

---
+++ Sensor_Test_Functions/Misc/Buzzer_Test/Buzzer_Test.ino
@@ -0,0 +1,14 @@
+int piezoPin = 14;
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  tone(piezoPin,4000,10000);
+  
+ 
+}
