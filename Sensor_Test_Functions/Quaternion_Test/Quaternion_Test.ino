e57ede980afb736328fb36e9aa9cfcc26e1144f0
Rita Tagarao <rtagarao20@gmail.com>
2021-03-06 15:59:17 -0700

    Created Quaternion_Test.ino
    Possible to determine position of a defined point based on quaternions

---
+++ Sensor_Test_Functions/Quaternion_Test/Quaternion_Test.ino
@@ -0,0 +1,102 @@
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BNO055.h>
+#include <utility/imumaths.h>
+#include <Servo.h>
+
+#define SERVO_PIN_PITCH 0
+#define SERVO_PIN_YAW 1
+
+Adafruit_BNO055 bno = Adafruit_BNO055(55);
+
+Servo servo_pitch;
+Servo servo_yaw;
+
+float pitch;
+float yaw;
+
+// Declare orientation array: [check, degX, degY, degZ] 
+float orient[] = {0,0,0,0};
+float orient_cur[3] = {0,0,0};
+float orient_last[3] = {0,0,0};
+
+void setup(void) 
+{
+  servo_pitch.attach(SERVO_PIN_PITCH);
+  servo_yaw.attach(SERVO_PIN_YAW);
+  servo_pitch.writeMicroseconds(1500);
+  servo_yaw.writeMicroseconds(1500);
+  
+  Serial.begin(9600);
+  /* Initialise the sensor */
+  if(!bno.begin())
+  {
+    /* There was a problem detecting the BNO055 ... check your connections */
+    Serial.print("Ooops, no BNO055 detected ... Check your wiring or I2C ADDR!");
+    while(1);
+  } 
+  orient[0] = 1;
+  delay(1000);   
+  bno.setExtCrystalUse(true);
+}
+
+imu::Quaternion getInverse(imu::Quaternion q) {
+  imu::Quaternion conj;
+  conj.w() = q.w();
+  conj.x() = -q.x();
+  conj.y() = -q.y();
+  conj.z() = -q.z();
+
+  double norm = sqrt(q.w()*q.w() + q.x()*q.x() + q.y()*q.y() + q.z()*q.z());
+
+  imu::Quaternion inv;
+
+  inv.w() = conj.w()/norm;
+  inv.x() = conj.x()/norm;
+  inv.y() = conj.y()/norm;
+  inv.z() = conj.z()/norm;
+
+  return inv;
+}
+
+void loop(void) 
+{
+  /* Angle data */
+  imu::Quaternion quat = bno.getQuat();
+  imu::Quaternion qInv = getInverse(quat);
+  float w = quat.w();
+  float x = quat.x(); //yaw 
+  float y = quat.y(); //pitch
+  float z = quat.z(); //roll
+  Serial.printf("q = [%5f %5f %5f %5f]         ", w,x,y,z);
+
+  imu::Quaternion pt;
+  pt.w() = 0;
+  pt.x() = 1;
+  pt.y() = 0;
+  pt.z() = 0;
+
+  imu::Quaternion rotatedP = quat * pt * qInv;
+  
+  Serial.printf("P = [%5f %5f %5f %5f]\n", rotatedP.w(),rotatedP.x(),rotatedP.y(),rotatedP.z());
+
+  //x,y,z define axis of rotation
+    //To rotate about a specified axis, only change that axis and w or only the other two
+      //Ex: Rotate about x - w & x can change OR y & z can change
+  //quaternion also described as q = cos(theta) + sin(theta)*(x,y,z)
+    //theta = angle/amount of rotation, (x,y,z) described above
+    
+   // 1: Find original axis of orientation
+   // 2: Find desired axis to base new quaternion on
+   // 3: Determine how much to move by
+
+
+  /*
+   * /total = local_rotation * total //multiplication order matters on this line
+   * //axis is a unit vector
+   * local_rotation.w = cosf( fAngle/2)
+   * local_rotation.x = axis.x * sinf( fAngle/2 )
+   * local_rotation.y = axis.y * sinf( fAngle/2 )
+   * local_rotation.z = axis.z * sinf( fAngle/2 )
+   */
+}
