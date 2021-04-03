13dfa7d0fc94b94c6aa859bf6f81ff5c256fdf9b
Rita Tagarao <rtagarao20@gmail.com>
2021-04-03 15:37:48 -0700

    Update BNO055_Test_Code.ino
    Drop test

--- Sensor_Test_Functions/BNO055_Test_Code/BNO055_Test_Code.ino
+++ Sensor_Test_Functions/BNO055_Test_Code/BNO055_Test_Code.ino
@@ -16,35 +16,64 @@ Adafruit_BNO055 bno = Adafruit_BNO055(55);
  */
 float orient[13];
   
-void setup(void) 
-{
-  
+void setup(void) {
   Serial.begin(9600);
-  Serial.println("Orientation Sensor Test"); Serial.println("");
-
-  orient[0] = 0;
-  
-  /* Initialise the sensor */
-  if(!bno.begin())
+  Serial.println("BNO055 Test");
+  Serial.println("");
+  if(!bno.begin(bno.OPERATION_MODE_NDOF)) //bno.OPERATION_MODE_IMUPLUS   bno.OPERATION_MODE_NDOF
   {
-    /* There was a problem detecting the BNO055 ... check your connections */
-    Serial.print("Ooops, no BNO055 detected ... Check your wiring or I2C ADDR!");
+    Serial.print("BNO055 failed to start");
     while(1);
-  } 
+  }
   
-  orient[0] = 1;
-  
-  delay(1000);
-    
   bno.setExtCrystalUse(true);
+
+//  pinMode(2,OUTPUT); //Pin 2 for BNO reset set as output
+
+  uint8_t cal, gyro, accel, mag = 0;
+  bno.getCalibration(&cal, &gyro, &accel, &mag);
+
+  Serial.print("Calibrating BNO055  ");
+  Serial.print(cal);
+  Serial.print("  ");
+  Serial.print(gyro);
+  Serial.print("  ");
+  Serial.print(accel);
+  Serial.print("  ");
+  Serial.println(mag);
+
+  while(cal != 3)
+  {
+    bno.getCalibration(&cal, &gyro, &accel, &mag);
+    Serial.print("Calibrating BNO055  ");
+    Serial.print(cal);
+    Serial.print("  ");
+    Serial.print(gyro);
+    Serial.print("  ");
+    Serial.print(accel);
+    Serial.print("  ");
+    Serial.println(mag);
+    delay(1000);
+  }
+
+  //Serial.println("BNO055 Calibrated and reset");
+
+//Attach and center servos
+//  s_pitch.attach(SERVO_PIN_PITCH);
+//  s_yaw.attach(SERVO_PIN_YAW);
+//  s_pitch.writeMicroseconds(1500);
+//  s_yaw.writeMicroseconds(1500);
+//  Serial.println("Servos attached and centered");
+
+  delay(1000);
 }
 
 void loop(void) 
 {
-  /* Angle data */
+  /* Angle data 
   imu::Vector<3> euler = bno.getVector(Adafruit_BNO055::VECTOR_EULER);
   
-  /* Display the floating point data of degrees*/
+  /* Display the floating point data of degrees
   Serial.printf("radX:%.2f",euler.x());
   Serial.printf("radY:%.2f",euler.y());
   Serial.printf("radZ:%.2f",euler.z());
@@ -53,24 +82,22 @@ void loop(void)
   orient[2] = euler.y();
   orient[3] = euler.z();
   delay(100);
-
+  */
+  
   /* Acceleration data */
   imu::Vector<3> accel = bno.getVector(Adafruit_BNO055::VECTOR_ACCELEROMETER);
   
   /* Display the floating point data m/s^2*/
-  Serial.printf("accelX:%.2f",accel.x());
-  Serial.printf("accelY:%.2f",accel.y());
-  Serial.printf("accelZ:%.2f",accel.z());
-  Serial.print("      ");
-  orient[4] = accel.x();
-  orient[5] = accel.y();
-  orient[6] = accel.z();
-  delay(100);
+  Serial.printf("%.2f   ",accel.x());
+  Serial.printf("%.2f   ",accel.y());
+  Serial.printf("%.2f   ",accel.z());
+  Serial.print("\n");
+
   
-  /* Gyro data */
+  /* Gyro data 
   imu::Vector<3> gyro = bno.getVector(Adafruit_BNO055::VECTOR_GYROSCOPE);
   
-  /* Display the floating point data rad/sec*/
+  /* Display the floating point data rad/sec
   Serial.printf("gyroX:%.2f",gyro.x());
   Serial.printf("gyroY:%.2f",gyro.y());
   Serial.printf("gyroZ:%.2f",gyro.y());
@@ -80,10 +107,10 @@ void loop(void)
   orient[9] = gyro.z();
   delay(100);
 
-  /* Magnetometer data */
+  /* Magnetometer data 
   imu::Vector<3> mag = bno.getVector(Adafruit_BNO055::VECTOR_MAGNETOMETER);
   
-  /* Display the floating point data micro Teslas*/
+  /* Display the floating point data micro Teslas
   Serial.printf("magX:%.2f",mag.x());
   Serial.printf("magY:%.2f",mag.y());
   Serial.printf("magZ:%.2f",mag.z());
@@ -92,4 +119,5 @@ void loop(void)
   orient[11] = mag.y();
   orient[12] = mag.z();
   delay(100);
+  */
 }
